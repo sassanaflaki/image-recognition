25d3453180ef65126aa2017190205138256b04cc
sassanaflaki <sassan.aflaki@gmail.com>
2025-07-30 22:38:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,24 +7,27 @@ from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 
-def count_fingers(hand_landmarks):
-    # Finger tip landmarks: Thumb(4), Index(8), Middle(12), Ring(16), Pinky(20)
-    tip_ids = [4, 8, 12, 16, 20]
+FINGER_TIPS = [4, 8, 12, 16, 20]
+FINGER_NAMES = ["Thumb", "Index", "Middle", "Ring", "Pinky"]
+
+def finger_status(hand_landmarks):
+    """Returns a list of raised fingers [Thumb, Index, Middle, Ring, Pinky]"""
     fingers = []
 
-    # Thumb
-    if hand_landmarks.landmark[tip_ids[0]].x < hand_landmarks.landmark[tip_ids[0] - 1].x:
+    # Thumb (horizontal check)
+    if hand_landmarks.landmark[FINGER_TIPS[0]].x < hand_landmarks.landmark[FINGER_TIPS[0] - 1].x:
         fingers.append(1)
     else:
         fingers.append(0)
 
-    # Other four fingers
-    for id in range(1, 5):
-        if hand_landmarks.landmark[tip_ids[id]].y < hand_landmarks.landmark[tip_ids[id] - 2].y:
+    # Other four fingers (vertical check)
+    for i in range(1, 5):
+        if hand_landmarks.landmark[FINGER_TIPS[i]].y < hand_landmarks.landmark[FINGER_TIPS[i] - 2].y:
             fingers.append(1)
         else:
             fingers.append(0)
-    return sum(fingers)
+
+    return fingers
 
 
 class VideoTransformer(VideoTransformerBase):
@@ -36,15 +39,18 @@ class VideoTransformer(VideoTransformerBase):
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = self.hands.process(img_rgb)
 
-        finger_count = 0
+        finger_text = "No hand detected"
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-                finger_count = count_fingers(hand_landmarks)
+                fingers = finger_status(hand_landmarks)
+                raised_fingers = [FINGER_NAMES[i] for i, val in enumerate(fingers) if val == 1]
+                finger_text = f"Fingers: {sum(fingers)} ({', '.join(raised_fingers)})"
 
-        cv2.putText(img, f'Fingers: {finger_count}', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+        cv2.putText(img, finger_text, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
         return img
 
 
-st.title("Real-Time Hand Detection & Finger Counting")
+st.title("Real-Time Hand & Finger Detection")
+st.write("Shows which fingers are raised (Thumb, Index, Middle, Ring, Pinky).")
 webrtc_streamer(key="finger-counter", video_transformer_factory=VideoTransformer)
